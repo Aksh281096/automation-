6d7d79cd61abb85554a949732ca9da72c3fc2386
Aksh281096 <94186569+Aksh281096@users.noreply.github.com>
2021-11-13 20:48:48 +0530

    fullname program
    by using string variable

---
+++ SecondExample.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+string firstname="akshay ";
+ string lastname="jaiswal";
+ cout<<"fullname:"<<firstname +lastname<<"\n";
+ cout<<"length of first name:"<<firstname.size()<<"\n";
+ cout<<"fullname:"<<firstname.append(lastname);
+return 0;
+}
